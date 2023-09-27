fad5bbd171f4484733f7e6e139e58d6c7863f9f9
ahmedtaha011246 <ahmedtaha011246@gmail.com>
2023-09-26 22:18:47 -0700

    calculator program

--- printt.c
+++ printt.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
- 
-int main()
-{
-    int num;
-    printf("enter an integer : ");
-    scanf("%d", &num);
-    printf("%d", num);
-    return 0;
+
+int main(){
+    int num1 , num2;
+    char operation;
+    printf("enter the first number: ");
+    scanf("%d", &num1);
+    printf("enter the second number: ");
+    scanf("%d", &num2);
+    printf("enter the operation: ");
+    scanf(" %c", &operation);
+    printf("the resault is: ");
+    if (operation == '+')
+        printf("%d", num1 + num2);
+    else if (operation == '-')
+        printf("%d", num1 - num2);
+    else if (operation == '*')
+        printf ("%d", num1 * num2);
+    else if (operation == '/' && num2 != 0)
+        printf ("%d", num1 / num2);
+    else
+        printf ("invalid input");
 }
